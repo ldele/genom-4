ac34542e833ae9c1716483813fc091413a0dd8b3
GTamo <tamo@SV-BBCF120.intranet.epfl.ch>
2016-10-10 11:29:10 +0200

    I added src and bin folders

---
+++ src/header.h
@@ -0,0 +1,11 @@
+# ifndef CLASS_H_
+# define CLASS_H_
+
+# include <string>
+# include <iostream>
+# include <map>
+
+// ------------- Declarations -------------
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+# include "header.h"
+
+// ------------- Declarations -------------
+
+// ------------- Main -------------
+int main(){
+	using namespace std;
+
+	std::cout << "Hello World" << std::endl;
+
+	return 0;
+};
+
+// ------------- Functions -------------
