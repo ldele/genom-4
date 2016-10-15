fc2e8ddcf85008bc1b484e7ef0d079023e90df70
Gianni <gianni.giusto@epfl.ch>
2016-10-15 18:08:34 +0200

    Documentation done

--- src/DNA.hpp
+++ src/DNA.hpp
@@ -3,30 +3,46 @@
 
 #include <string>
 
+/*! DNA class */
 
 class DNA
 {
 
 public:
-
+	
     //Constructeur par défaut
+    /*!
+     * Constructor
+     */
     DNA (string filename);
 
     //Ouvre un fichier et le lit
+     /*!
+     * Function 1
+     * Param filename 
+     */
     void openFromFile (string filename);
     
     //Retourne un segement avec une position de départ et une longeur donnée
+     /*!
+     * Function 2
+     * Param StartPos as first argument
+     * Param length as second argument
+     */
     string returnSeg (unsigned int StartPos, unsigned int length);
     
     
 private:
 
 	//Vérifie s'il n'y a que des bases (ATGC) dans la séquence d'ADN
+	 /*!
+     * Check
+     */
 	bool checkDNAString ();
 	
 	//Attributs
-	int size;
-	string mDNAseq;
+	int size; /*!< Size of the DNA strand */
+	string mDNAseq; /*!< Sequence of the DNA */
 	
 };
 
