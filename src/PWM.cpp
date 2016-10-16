4c3519d4b2a02e74e4e8b8985ef6be52b6f41cbe
rcucu <raluca-maria.cucu@epfl.ch>
2016-10-16 19:34:57 +0200

    added checkPWM to PWM file

--- src/PWM.cpp
+++ src/PWM.cpp
@@ -6,7 +6,30 @@ void PWM::openFromFile (string filename)
 	
 }
 
-bool PWM::checkPWM()
+//va etre appelée dans le corps du constructeur de PWM afin de vérifier
+//les valeurs placées dans le tableau
+void PWM::checkPWM()
 {
-	
+	for (auto ligne : mPWM){
+		for (auto prob : ligne){
+				try{
+					if (prob >= 0.0){
+						try{
+							if (prob >= 1.0){
+									throw string ("valeur introduite supérieure à 1.0");
+									}	
+							}
+						catch (string const& erreur){
+							cerr<< "Erreur : " <<erreur<<endl;
+							}
+					}
+					else{
+						throw string ("valeur introduite est negative");	
+						}
+					}	
+				catch (string const& erreur){
+					cerr<< "Erreur : "<<erreur<<endl;
+					}						
+			}
+	}		
 }
--- src/PWM.hpp
+++ src/PWM.hpp
@@ -3,38 +3,23 @@
 
 #include <vector>
 
-/*! PWM class */
-
 class PWM
 {
 public:
 
 	//constructeur de PWM
-	/*!
-     * Constructor
-     */
 	PWM(string filename); 
 	
 	//fonction qui lit le fichier
-	/*!
-     * Function 1
-     * Param filename 
-     */
 	void openFromFile (string filename); 
 	
 	
 private:
 
 	//verification de la matrice
-	/*!
-     * Private function
-     * Check PWM file
-     */
-	bool checkPWM(); 
+	void checkPWM(); 
 		
 	// matrice de score
-	vector<vector<double>> mPWM; /*!< PWM matrix */
+	vector<vector<double>> mPWM;  
 
-};
-
-#endif
+}
