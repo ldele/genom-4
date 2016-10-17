65ae3afda2968d8f85dd15e48510bbe1f2d5aa90
tvuignie <terry.vuignier@epfl.ch>
2016-10-17 11:49:23 +0200

    checkDNA fait

--- src/DNA.cpp
+++ src/DNA.cpp
@@ -18,3 +18,15 @@ string DNA::returnSeg (unsigned int StartPos, unsigned int length){
 	
 }
 
+bool checkDNAstring(std::string mDNAseq1){   //methode permettant de lire la sequence et de controler 
+	                                         //si cette dernière ne contient pas d'erreur
+
+size_t found = mDNAseq1.find_first_not_of("ATGC");   //fonction qui retourne la position de la première lettre qui n'est pas un ATGC
+                                                     //retourne string::npos si toutes les lettres sont ATGC
+
+if (found!=std::string::npos) {
+	return false;
+	} else {
+		return true;
+	}
+}
