76bc838e731aabb61b1e61f7b3d16f535149464f
Florent <florent.sierro@epfl.ch>
2016-10-26 17:20:20 +0200

    unified DNA whitout openfromFile and read DNA fstream

--- src/DNA.cpp
+++ src/DNA.cpp
@@ -2,31 +2,32 @@
 #include <string>
 
 
-
-DNA::DNA (string filename)
+DNA::DNA (std::string filename)
 {
     openFromFile(filename);
 }
 
 
-void DNA::openFromFile (string filename){
+void DNA::openFromFile (std::string filename){
 	
 }
    
-    
-string DNA::returnSeg (unsigned int StartPos, unsigned int length){
-	
+
+std::string DNA::returnSeg (unsigned int startpos, unsigned int lenght ){
+
+	return mDNAseq.substr(startpos, lenght);
+
+} 
+
+
+bool DNA::checkDNAString(std::string mDNAseq1){  		//methode permettant de lire la sequence et de controler 
+														//si cette dernière ne contient pas d'erreur
+	size_t found = mDNAseq1.find_first_not_of("ATGC");  //fonction qui retourne la position de la première lettre qui n'est pas un ATGC
+														//retourne string::npos si toutes les lettres sont ATGC
+	if (found!=std::string::npos) {
+		return false;
+		} else {
+			return true;
+		}
 }
 
-bool checkDNAstring(std::string mDNAseq1){   //methode permettant de lire la sequence et de controler 
-	                                         //si cette dernière ne contient pas d'erreur
-
-size_t found = mDNAseq1.find_first_not_of("ATGC");   //fonction qui retourne la position de la première lettre qui n'est pas un ATGC
-                                                     //retourne string::npos si toutes les lettres sont ATGC
-
-if (found!=std::string::npos) {
-	return false;
-	} else {
-		return true;
-	}
-}
