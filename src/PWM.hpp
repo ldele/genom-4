eb6d06225cd14cedd384f2563098aac3e7cae9e5
Gianni <gianni.giusto@epfl.ch>
2016-10-30 14:17:34 +0100

    Doc checked and modified

--- src/PWM.hpp
+++ src/PWM.hpp
@@ -30,16 +30,29 @@ private:
 	void checkPWM();
 =======
 
-	//check si c'est la valeur introduite est une lettre
+	/*!
+     * Private function
+     * Check si c'est la valeur introduite est une lettre
+     */
 	void checkLetter (double score);
 	
-	//check si valeur est comprise entre 0 et 1
+	/*!
+     * Private function
+     * Check si valeur est comprise entre 0 et 1
+     */
 	void checkscorePWM(double score);
 	
-	//checker si le log est compris entre -infini et 0
+	
+	/*!
+     * Private function
+     * Check si le log est compris entre -infini et 0
+     */
 	void checkscorePSSM(double score);
 	
-	//check si la somme des valeurs d'une ligne de PWM est = 1
+	/*!
+     * Private function
+     * check si la somme des valeurs d'une ligne de PWM est = 1
+     */
 	void checkLigne();
 >>>>>>> 547f0b7c1e0a1497f74161b67b117e7ad5593bf1
 		
