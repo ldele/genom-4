db73d7a74e41f2cc436e1b700c935aea27436991
Gianni <gianni.giusto@epfl.ch>
2016-10-31 08:49:03 +0100

    Doc updated

--- src/PWM.hpp
+++ src/PWM.hpp
@@ -22,27 +22,23 @@ public:
 	
 	
 private:
+
+	
 <<<<<<< HEAD
-	/*!
-     * Private function
-     * Check PWM file - Vérification de la matrice
-     */
-	void checkPWM();
 =======
 
 	/*!
      * Private function
-     * Check si c'est la valeur introduite est une lettre
+     * Check si la valeur introduite est une lettre ou un caractère - Vérification de la matrice
      */
-	void checkLetter (double score);
+	bool checkLetter(const char* filename);
 	
 	/*!
      * Private function
      * Check si valeur est comprise entre 0 et 1
      */
 	void checkscorePWM(double score);
-	
-	
+	 
 	/*!
      * Private function
      * Check si le log est compris entre -infini et 0
@@ -51,12 +47,45 @@ private:
 	
 	/*!
      * Private function
-     * check si la somme des valeurs d'une ligne de PWM est = 1
+     * Check si la somme des valeurs d'une ligne de PWM est = 1
      */
-	void checkLigne();
+	void checkLigne(double somme, unsigned int l);
+	
+	/*!
+     * Private function
+     * Methode qui transforme la matrice PPM a (PPM / la constante (plus grande valeur de la matrice))
+     */
+	void transfoPPMC();
+	
+	/*!
+     * Private function
+     * Methode qui transforme la matrice PPM a log(PPM)
+     */
+	void transfoPSSM();
+	
+	/*!
+     * Private function
+     * Methode qui transforme la matrice PSSM a exp(PSSM)
+     */
+	void transfoPPM();
+	
+	/*!
+     * Private function
+     * Methode qui transforme la matrice PSSM a (PSSM - la constante (plus grande valeur de la matrice))
+     */
+	void transfoPSSMC();
+	
+	
 >>>>>>> 547f0b7c1e0a1497f74161b67b117e7ad5593bf1
+	
 		
 	vector<vector<double>> mPWM; /*!< Matrice du score */
+	
+	vector<vector<double>> mPPMC; /*!< Matrice obtenue en faisant PPM / la constante (plus grande valeur de la matrice) */
+
+	vector<vector<double>> mPSSM; /*!< Matrice obtenue en faisant log(PPM) */
+	
+	vector<vector<double>> mPSSMC; /*!< Matrice obtenue en faisant PSSM -la constante (plus grande valeur de la matrice) */
 
 };
 
